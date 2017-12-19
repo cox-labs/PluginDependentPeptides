19c93d74a7b2b76e9102b78ec9803bb35a1f53e4
Jan Rudolph <rudolph@biochem.mpg.de>
2017-12-19 13:23:41 +0100

    improve perseuspy version mismatch error message; update dependencies; version 1.6.1.0

--- PluginDependentPeptides/Resources/dependent_peptides.py
+++ PluginDependentPeptides/Resources/dependent_peptides.py
@@ -6,9 +6,10 @@ try:
 	from perseuspy import __version__
 except ImportError:
 	__version__ = "0.1.0"
-expected_version = "0.1.3"
+expected_version = "0.3.3"
 if __version__ != expected_version:
-	print("Found unsupported `perseuspy` version {}".format(__version__))
+	print("Please make sure you correctly exported the RAW files table from MaxQuant.")
+	print("Additionally found unexpected `perseuspy` version {}, which might be causing the error.".format(__version__))
 	print("Make sure to install `perseuspy` version {}".format(expected_version))
 import sys
 from perseuspy.dependent_peptides import run_dependent_peptides_from_parameters
--- PluginDependentPeptides/Upload.cs
+++ PluginDependentPeptides/Upload.cs
@@ -7,24 +7,18 @@ using Path = System.IO.Path;
 
 namespace PluginDependentPeptides
 {
-    public class Upload : PluginInterop.MatrixUpload
+    public class Upload : PluginInterop.Python.MatrixUpload
     {
         public override string Name => "Dependent peptides";
-        public override string Description => "Create the dependent peptides table from the allPeptides.txt and experimentalDesign.txt";
+        public override string Description => "Create the dependent peptides table from the allPeptides.txt and rawFilesTable.txt";
 
         public override Bitmap2 DisplayImage => GraphUtils.ToBitmap2(Properties.Resources.dependent_peptides_icon);
 
-        protected override string GetCodeFile(Parameters param)
+        protected override bool TryGetCodeFile(Parameters param, out string codeFile)
         {
-            byte[] code = (byte[])Properties.Resources.ResourceManager.GetObject("dependent_peptides");
-            var codeFile = Path.GetTempFileName();
-            File.WriteAllText(codeFile, Encoding.Default.GetString(code, 0, code.Length - 1));
-            return codeFile;
-        }
-
-        protected override bool TryFindExecutable(out string path)
-        {
-            return PluginInterop.Python.Utils.TryFindPythonExecutable(out path);
+            codeFile = Path.GetTempFileName();
+            File.WriteAllText(codeFile, Encoding.UTF8.GetString(Properties.Resources.dependent_peptides));
+            return true;
         }
 
         protected override Parameter[] SpecificParameters(ref string errString)
